88859c5a51389c9eaf9243577b1244de38d7a0b8
milk19948 <97678058+Milk25@users.noreply.github.com>
2024-07-21 14:28:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let num1 = 5
+let num2 = 3
+
+
+let sum = num1 + num2
+
+let difference = num1 - num2
+
+let product = num1 * num2
+
+let quotient = num1 / num2
+
+console.log("Sum:", sum)
+console.log("Difference:", difference)
+console.log("Product:", product)
+console.log("Quotient:", quotient)
+
+
+num1 = 10
+num2 = 45
+console.log("Updated num1:", num1)
+console.log("Updated num2:", num2)
+
+
+numbers = (num1 === num2)
+numbers2 = (num1 > num2)
+numbers3 = (num1 != num2)
+
+console.log("Is num1 equal to num2?",  numbers)
+console.log("Is num1 greater than num2?", numbers2)
+console.log("Is num1 not equal to num2?", numbers3)
+
+
+let isPositive = true
+let isEven = true
+console.log("Are bth numbers positive?", isPositive)
+console.log("Is at least one number even?", isEven)
+
